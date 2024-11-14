5bb74b641025d1b7f1da1e4cb142b11cc390292e
dhruvupa <upadhyaydhruv6@gmail.com>
2024-11-14 17:40:10 +0000

    Added Add service UI, List service Functionality, And fix URL path redirection

--- src/main/java/com/example/demo/Controller/SearchServiceController.java
+++ src/main/java/com/example/demo/Controller/SearchServiceController.java
@@ -10,45 +10,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.example.demo.Model.Customer.Customer;
+import com.example.demo.GlobalContext;
 import com.example.demo.Model.SearchService.SearchService;
-import com.example.demo.Model.User.IUserFactory;
-import com.example.demo.Model.User.User;
-import com.example.demo.Service.SearchServiceService;
-import com.example.demo.Service.ServiceProvider_Service;
+import com.example.demo.ServiceProvider.Service.ServiceProviderService;
+
 
 @Controller
-@RequestMapping("/serviceProvider")
+@RequestMapping("/Provider")
 public class SearchServiceController {
 
-	
-   //private  final SearchService searchService;
-    private final SearchServiceService searchServiceService;
-    
-    public SearchServiceController(SearchServiceService searchServiceService) {
-        this.searchServiceService = searchServiceService;
-       // this.searchService=searchService;
-    }
-    
 
-    @GetMapping("/serviceProviderWelcomeScreen")
-    public String serviceProviderForm(Model model){
-    	return "ServiceProviderWelcomeScreen";
-	}
-    
-    @GetMapping("/addServiceForm")
-    //@ResponseBody
-    public String addServiceForm(Model model){
-    	model.addAttribute("SearchService",new SearchService());
-    	return "addServiceForm";
-    }
-    
-    @PostMapping("/addServiceItem")
-    @ResponseBody
-    public String addServiceItem(SearchService SearchService) throws SQLException {
-    	
-    	System.out.print("Done dona Done");
-    	return "sswfds";
-    }
-
+   
 }
--- src/main/java/com/example/demo/Controller/ServiceProvider_Controller.java
+++ src/main/java/com/example/demo/Controller/ServiceProvider_Controller.java
@@ -1,15 +1,17 @@
 package com.example.demo.Controller;
 
 
+import com.example.demo.GlobalContext;
+import com.example.demo.Model.SearchService.SearchService;
 import com.example.demo.Model.ServiceProvider.ServiceProvider;
 import com.example.demo.Model.User.IUserFactory;
 import com.example.demo.Model.User.User;
 import com.example.demo.ServiceProvider.Service.ServiceProviderService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.sql.SQLException;
 
@@ -19,10 +21,13 @@ public class ServiceProvider_Controller {
 
     private final ServiceProviderService serviceproviderservice;
     private final IUserFactory userFactory;
+    
+	private  final GlobalContext globalcontext; 
 
-    public ServiceProvider_Controller(ServiceProviderService serviceproviderservice, IUserFactory userFactory) {
+    public ServiceProvider_Controller(ServiceProviderService serviceproviderservice, IUserFactory userFactory,GlobalContext globalcontext) {
         this.serviceproviderservice = serviceproviderservice;
         this.userFactory = userFactory;
+        this.globalcontext=globalcontext;
     }
 
     // Endpoint to register a new service provider
@@ -58,17 +63,46 @@ public class ServiceProvider_Controller {
 
     @PostMapping("/loginsucess")
     @ResponseBody
-    public String loginServiceProvider(@RequestParam String email, @RequestParam String password) {
+    public RedirectView loginServiceProvider(@RequestParam String email, @RequestParam String password,RedirectAttributes redirectAttributes) {
         boolean isAuthenticated = serviceproviderservice.validateLogin(email, password);
         if (isAuthenticated) {
-            return "Login Successful";
+        	return new RedirectView("ServiceProviderWelcomeScreen");
+            //return "Login Successful";
 //            model.addAttribute("email", email);
 //            return "ServiceProviderDashboard";
         } else {
-            return "Invalid email or password";
+            //return "Invalid email or password";
+        	 redirectAttributes.addFlashAttribute("error", "Invalid email or password");
+             return new RedirectView("ServiceProviderLoginForm");
         }
     }
 
+    @GetMapping("/ServiceProviderWelcomeScreen")
+    public String serviceProviderForm(Model model){
+    	return "ServiceProviderWelcomeScreen";
+	}
+    
+    @GetMapping("/addServiceForm")
+    //@ResponseBody
+    public String addServiceForm(Model model){
+    	model.addAttribute("SearchService",new SearchService());
+    	return "addServiceForm";
+    }
+    
+    @PostMapping("/addServiceItem")
+    @ResponseBody
+    public String addServiceItem(SearchService SearchService) throws SQLException {
+    	int s = serviceproviderservice.verifyServiceAddition(SearchService);
+    	System.out.print("Done dona Done");
+    	return "sswfds";
+    }
 
+    @GetMapping("/ListServices")
+    //@ResponseBody
+    public String listServices(Model model){
+    	System.out.println(globalcontext.getServiceProviderId());
+    	model.addAttribute("SearchService",serviceproviderservice.getAllServiceProviderServices());
+    	return "ListServices";
+    }
 
 }
