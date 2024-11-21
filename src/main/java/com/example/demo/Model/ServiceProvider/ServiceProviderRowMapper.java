8522ced582c90be22ab01f56f426f502fbd5e308
Achyutam <Achyutam.ire@gmail.com>
2024-11-21 19:45:25 +0000

    Bug fix

--- src/main/java/com/example/demo/Model/ServiceProvider/ServiceProviderRowMapper.java
+++ src/main/java/com/example/demo/Model/ServiceProvider/ServiceProviderRowMapper.java
@@ -16,12 +16,8 @@ public class ServiceProviderRowMapper implements RowMapper<ServiceProvider>{
 		 ServiceProvider serviceProvider = new ServiceProvider();
 		 serviceProvider.setName(rs.getString("provider_name")); 
 		 serviceProvider.setCity(rs.getString("city"));
-		 serviceProvider.setEmail(rs.getString("email")); 
-<<<<<<< Updated upstream
-		 serviceProvider.setStatus(rs.getString("status")); 
-=======
+		 serviceProvider.setEmail(rs.getString("email"));
 		 serviceProvider.setStatus(rs.getString("status"));
->>>>>>> Stashed changes
 		 serviceProvider.setProviderId(rs.getString("provider_id"));		
 		return serviceProvider;
 	    }
